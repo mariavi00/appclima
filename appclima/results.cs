57bc4a665a737dcb513b4d668019dec021060786
appclima <mavi.gouveia678@gmail.com>
2024-04-18 09:03:24 -0300

    18/04

---
+++ appclima/results.cs
@@ -0,0 +1,11 @@
+
+ namespace appclima;
+ public class results
+ {
+    public int Temp {get;set;} 
+    public string description {get; set;}
+    public string condition_code {get; set;}
+    public string img_id {get; set;}
+    public string city {get; set;}
+    
+ }
